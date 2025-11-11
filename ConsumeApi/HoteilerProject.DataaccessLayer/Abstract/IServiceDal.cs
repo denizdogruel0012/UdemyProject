a9721f78b07e6722b58f1a041d65dcbaabe2107f
denizdogruel00 <denizyazilm@gmail.com>
2025-11-11 13:10:13 +0300

    abstract sınıfı bitti

---
+++ ConsumeApi/HoteilerProject.DataaccessLayer/Abstract/IServiceDal.cs
@@ -0,0 +1,13 @@
+﻿using HoteilerProject.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoteilerProject.DataaccessLayer.Abstract
+{
+    public interface IServiceDal:IGenericDal<Service>
+    {
+    }
+}
