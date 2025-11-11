24f7333e98ef9cfd6ced72773f47add2ca878472
denizdogruel00 <denizyazilm@gmail.com>
2025-11-11 13:56:24 +0300

    businesslayer abstract klasörü bitti

---
+++ ConsumeApi/HoteilerProject.BusinessLayer/Abstract/ISubscribeService.cs
@@ -0,0 +1,13 @@
+﻿using HoteilerProject.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoteilerProject.BusinessLayer.Abstract
+{
+    public interface ISubscribeService:IGenericService<Subscribe>
+    {
+    }
+}
