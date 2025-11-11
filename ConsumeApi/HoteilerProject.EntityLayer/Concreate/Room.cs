05b1b24749042cacaa567b147507f31105dd9d88
denizdogruel00 <denizyazilm@gmail.com>
2025-11-11 12:43:17 +0300

    katmanlar ve varlıklar eklendi

--- ConsumeApi/HoteilerProject.EntityLayer/Concreate/Room.cs
+++ ConsumeApi/HoteilerProject.EntityLayer/Concreate/Room.cs
@@ -10,12 +10,14 @@ namespace HoteilerProject.EntityLayer.Concreate
     {
 
         public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
         public string CoverImage { get; set; }
         public int Price { get; set; }
         public string Title { get; set; }
         public string BedCount{ get; set; }
         public string BathCount { get; set; }
         public string wifi { get; set; }
+        public string description { get; set; }
 
 
     }
