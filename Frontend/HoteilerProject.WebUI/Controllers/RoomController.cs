b2fc173cab4594adaaf6989805ad3d8d38c0d38e
denizdogruel00 <denizyazilm@gmail.com>
2025-11-12 16:21:09 +0300

    roomcontroller eklendi

--- Frontend/HoteilerProject.WebUI/Controllers/RoomController.cs
+++ Frontend/HoteilerProject.WebUI/Controllers/RoomController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HoteilerProject.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HoteilerProject.WebUI.Controllers
 {
     public class RoomController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = client.GetAsync("http://localhost:5195/api/Room").Result;
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var Json=responseMessage.Content.ReadAsStringAsync().Result;
+                var values = Newtonsoft.Json.JsonConvert.DeserializeObject<List<RoomWithModel>>(Json);
+                return View(values);
+            }
+
             return View();
         }
     }
---
+++ Frontend/HoteilerProject.WebUI/Models/RoomWithModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HoteilerProject.WebUI.Models
+{
+    public class RoomWithModel
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string CoverImage { get; set; }
+        public int Price { get; set; }
+        public string Title { get; set; }
+        public string BedCount { get; set; }
+        public string BathCount { get; set; }
+        public string wifi { get; set; }
+        public string description { get; set; }
+    }
+}
