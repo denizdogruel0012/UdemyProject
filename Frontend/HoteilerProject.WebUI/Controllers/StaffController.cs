8a6e1859a697c2d5e6aedd27eccc611d343c8a57
denizdogruel00 <denizyazilm@gmail.com>
2025-11-12 13:15:20 +0300

    deserialize işlemleri ve personeller listelendi

--- Frontend/HoteilerProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HoteilerProject.WebUI/Controllers/StaffController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HoteilerProject.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HoteilerProject.WebUI.Controllers
 {
     public class StaffController : Controller
-    {
-        public IActionResult Index()
+    {private readonly IHttpClientFactory _httpClientFactory;
+
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5195/api/Staff");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var Json=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<StaffWithModel>>(Json);
+                return View(values);
+            }
+            
+
             return View();
         }
     }
---
+++ Frontend/HoteilerProject.WebUI/Models/StaffWithModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HoteilerProject.WebUI.Models
+{
+    public class StaffWithModel
+    {
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
