e0b6b17923cd51fbcb1eacdb902fb9514b6cef90
denizdogruel00 <denizyazilm@gmail.com>
2025-11-12 09:55:39 +0300

    layout eklendi

---
+++ Frontend/HoteilerProject.WebUI/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HoteilerProject.WebUI.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
